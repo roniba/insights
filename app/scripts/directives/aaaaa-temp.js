c68f16e612acf16957d7b7f1ca196b33e4538e0f
Roni Ben-Aharon <roniba@wix.com>
2016-04-21 12:47:03 +0300

    trying to make the chart working - SOW

--- app/scripts/directives/aaaaa-temp.js
+++ app/scripts/directives/aaaaa-temp.js
@@ -1,17 +1,16 @@
-//angular.module("insightsApp")
 angular.module("insightsApp", ["chart.js"])
    //Optional configuration
-  //.config(['ChartJsProvider', function (ChartJsProvider) {
-  //  // Configure all charts
-  //  ChartJsProvider.setOptions({
-  //    colours: ['#FF5252', '#FF8A80'],
-  //    responsive: false
-  //  });
-  //  // Configure all line charts
-  //  ChartJsProvider.setOptions('Line', {
-  //    datasetFill: false
-  //  });
-  //}])
+  .config(['ChartJsProvider', function (ChartJsProvider) {
+    // Configure all charts
+    ChartJsProvider.setOptions({
+      colours: ['#FF5252', '#FF8A80'],
+      responsive: false
+    });
+    // Configure all line charts
+    ChartJsProvider.setOptions('Line', {
+      datasetFill: false
+    });
+  }])
   .controller("LineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
