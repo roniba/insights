7eb5c83d5940db9a23c504289064ac6d432121d1
Roni Ben-Aharon <roniba@wix.com>
2016-04-24 11:05:33 +0300

    overview-graphs is now a proper directive

--- app/scripts/directives/overview-graphs.js
+++ app/scripts/directives/overview-graphs.js
@@ -1,50 +1,32 @@
-//angular.module('insightsApp')
-//  .directive('button1', function () {
-//    return {
-//      restrict:    'E',
-//      templateUrl: 'views/button1.html',
-//      scope:       {
-//        label:   '@',
-//        onClick: '&'
-//      }
-//    };
-//  });
-
-angular.module('insightsApp').controller("HelloWorldController", function ($scope) {
-
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-});
-
-angular.module('insightsApp').controller("BarCtrl", function ($scope) {
-  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.series = ['Series A', 'Series B'];
-
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-});
-
 angular.module('insightsApp')
+  .directive('overviewGraphs', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'views/overview-graphs.html',
+      scope: {},
+      controllerAs : '$ctrl',
+      controller: function() {
+        this.ordersGraph = {};
+        this.ordersGraph.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        this.ordersGraph.series = ['Series A', 'Series B'];
+        this.ordersGraph.data = [
+          [65, 59, 80, 81, 56, 55, 40],
+          [28, 48, 40, 19, 86, 27, 90]
+        ];
+        this.ordersGraph.onClick = function (points, evt) {
+          console.log(points, evt);
+        };
 
-  .controller('GraphCtrl', function($scope) {
-    $scope.graph = {};
-    $scope.graph.name = 'myGraph';
-    $scope.graph.data = [
-      //Awake
-      [16, 15, 20, 12, 16, 12, 8],
-      //Asleep
-      [8, 9, 4, 12, 8, 12, 14]
-    ];
-    $scope.graph.labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    $scope.graph.series = ['Awake', 'Asleep'];
-
+        this.revenueGraph = {};
+        this.revenueGraph.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        this.revenueGraph.series = ['Series C', 'Series D'];
+        this.revenueGraph.data = [
+          [65, 59, 80, 81, 56, 55, 40],
+          [28, 48, 40, 19, 86, 27, 90]
+        ];
+        this.revenueGraph.onClick = function (points, evt) {
+          console.log(points, evt);
+        };
+      }
+    }
   });
