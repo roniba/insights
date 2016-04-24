14f3572a842027aa764c1b85fe8566360e363b78
Roni Ben-Aharon <roniba@wix.com>
2016-04-24 14:57:00 +0300

    cleanup

--- app/scripts/directives/overview-graphs.js
+++ app/scripts/directives/overview-graphs.js
@@ -4,31 +4,6 @@ angular.module('insightsApp')
   .directive('overviewGraphs', function () {
     return {
       restrict: 'E',
-      templateUrl: 'views/overview-graphs.html',
-      scope: {},
-      controllerAs: '$ctrl',
-      controller: function () {
-        this.ordersGraph = {};
-        this.ordersGraph.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-        this.ordersGraph.series = ['Series A', 'Series B'];
-        this.ordersGraph.data = [
-          [65, 59, 80, 81, 56, 55, 40],
-          [28, 48, 40, 19, 86, 27, 90]
-        ];
-        this.ordersGraph.onClick = function (points, evt) {
-          console.log(points, evt);
-        };
-
-        this.revenueGraph = {};
-        this.revenueGraph.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-        this.revenueGraph.series = ['Series C', 'Series D'];
-        this.revenueGraph.data = [
-          [65, 59, 80, 81, 56, 55, 40],
-          [28, 48, 40, 19, 86, 27, 90]
-        ];
-        this.revenueGraph.onClick = function (points, evt) {
-          console.log(points, evt);
-        };
-      }
+      templateUrl: 'views/overview-graphs.html'
     }
   });
