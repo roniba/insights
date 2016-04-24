904740605bb44231839347ca67409873604ba11d
Roni Ben-Aharon <roniba@wix.com>
2016-04-24 10:51:30 +0300

    angular-charts seem to work

--- app/scripts/directives/aaaaa-temp.js
+++ app/scripts/directives/aaaaa-temp.js
@@ -1,33 +1,38 @@
-angular.module("insightsApp", ["chart.js"])
-   //Optional configuration
-  .config(['ChartJsProvider', function (ChartJsProvider) {
-    // Configure all charts
-    ChartJsProvider.setOptions({
-      colours: ['#FF5252', '#FF8A80'],
-      responsive: false
-    });
-    // Configure all line charts
-    ChartJsProvider.setOptions('Line', {
-      datasetFill: false
-    });
-  }])
-  .controller("LineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
+angular.module('insightsApp').controller("HelloWorldController", function ($scope) {
 
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+});
+
+angular.module('insightsApp').controller("BarCtrl", function ($scope) {
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+});
+
+angular.module('insightsApp')
+
+  .controller('GraphCtrl', function($scope) {
+    $scope.graph = {};
+    $scope.graph.name = 'myGraph';
+    $scope.graph.data = [
+      //Awake
+      [16, 15, 20, 12, 16, 12, 8],
+      //Asleep
+      [8, 9, 4, 12, 8, 12, 14]
     ];
-    $scope.onClick = function (points, evt) {
-      console.log(points, evt);
-    };
+    $scope.graph.labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    $scope.graph.series = ['Awake', 'Asleep'];
 
-    // Simulate async data update
-    $timeout(function () {
-      $scope.data = [
-        [28, 48, 40, 19, 86, 27, 90],
-        [65, 59, 80, 81, 56, 55, 40]
-      ];
-    }, 3000);
-  }]);
+  });
